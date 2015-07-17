8da65ff94754b7bb566e75ce31396af614db2108
Slava Shklyaev <shk.slava@gmail.com>
2015-07-17 18:29:28 +0300

    Add chat models

--- django/chat/models.py
+++ django/chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Conversation(models.Model):
+    name = models.CharField(max_length=50)
+    members = models.ManyToMany(settings.AUTH_USER_MODEL,
+                                related_name='conversations')
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    text = models.TextField()
+    conversation = models.ForeignKey(Conversation, related_name='messages')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.text
