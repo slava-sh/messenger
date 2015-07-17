29e0f6341b885c1535697377916550cacb9bb8a9
Slava Shklyaev <shk.slava@gmail.com>
2015-07-17 18:30:49 +0300

    Clean up

--- django/chat/views.py
+++ django/chat/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 
+
 def home(request):
     return HttpResponse('hey<br>\n{}'.format(reverse('chat:home')))
