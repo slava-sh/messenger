977fff5cca7e99f0b991b1b82ea26f4125323f46
Slava Shklyaev <shk.slava@gmail.com>
2015-07-21 22:37:09 +0300

    Minor improvements

--- django/chat/urls.py
+++ django/chat/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^c/$', views.conversation_list, name='conversation_list'),
     url(r'^c/(?P<pk>[0-9]+)/$', views.conversation, name='conversation'),
     url(r'^c/new/$', views.create_conversation, name='create_conversation'),
 ]
