a0558c352cd93c854fd68f785e3360fed1e82fbc
Slava Shklyaev <shk.slava@gmail.com>
2015-08-13 12:48:51 +0300

    Migrate to SockJS

--- realtime/index.js
+++ realtime/index.js
@@ -1,54 +1,72 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var http = require('http');
+var sockjs = require('sockjs');
 var amqp = require('amqp');
 
 var port = process.env.PORT || 3000;
+var sockOptions = {
+  sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js',
+};
 
-server.listen(port, function() {
-  console.log('Server listening at port %d', port);
-});
+var sockServer = sockjs.createServer(sockOptions);
+sockServer.on('connection', handleConnection);
 
-var sockets = {};
+var server = http.createServer();
+server.addListener('upgrade', endRequest);
+sockServer.installHandlers(server);
 
-io.on('connection', function(socket) {
+console.log('Listening on port %d', port);
+server.listen(port);
 
-  socket.on('register', function(id) {
-    // if (sockets[id]) {
-    //   return;
-    // }
-    socket.client_id = id;
-    sockets[id] = socket;
-    console.log('register', id);
-    console.log('active clients:', Object.keys(sockets));
+function handleConnection(conn) {
+  conn.on('data', function(message) {
+    console.log('got', arguments);
+    conn.write(message);
   });
+}
 
-  socket.on('disconnect', function() {
-    if (!sockets[socket.client_id]) {
-      return;
-    }
-    console.log('disconnect', socket.client_id);
-    delete socket[socket.client_id];
-  });
-});
+function endRequest(req, res) {
+  res.end();
+}
 
-var connection = amqp.createConnection({ host: 'queue' });
-connection.on('ready', function() {
-  var queueOptions = { durable: true, autoDelete: false };
-  connection.queue('notifications', queueOptions, function(queue) {
-    queue.bind('#');
-    queue.subscribe(function(payload) {
-      var client_ids = payload.args[0].map(String);
-      var notification = payload.args[1];
-      console.log(client_ids, notification);
-      client_ids.forEach(function(client_id) {
-        var socket = sockets[client_id];
-        if (socket) {
-          socket.send(notification);
-          console.log('sent to', client_id);
-        }
-      });
-    });
-  });
-});
+//var sockets = {};
+//
+//io.on('connection', function(socket) {
+//
+//  socket.on('register', function(id) {
+//    // if (sockets[id]) {
+//    //   return;
+//    // }
+//    socket.client_id = id;
+//    sockets[id] = socket;
+//    console.log('register', id);
+//    console.log('active clients:', Object.keys(sockets));
+//  });
+//
+//  socket.on('disconnect', function() {
+//    if (!sockets[socket.client_id]) {
+//      return;
+//    }
+//    console.log('disconnect', socket.client_id);
+//    delete socket[socket.client_id];
+//  });
+//});
+//
+//var connection = amqp.createConnection({ host: 'queue' });
+//connection.on('ready', function() {
+//  var queueOptions = { durable: true, autoDelete: false };
+//  connection.queue('notifications', queueOptions, function(queue) {
+//    queue.bind('#');
+//    queue.subscribe(function(payload) {
+//      var client_ids = payload.args[0].map(String);
+//      var notification = payload.args[1];
+//      console.log(client_ids, notification);
+//      client_ids.forEach(function(client_id) {
+//        var socket = sockets[client_id];
+//        if (socket) {
+//          socket.send(notification);
+//          console.log('sent to', client_id);
+//        }
+//      });
+//    });
+//  });
+//});
--- web/react_chat/assets/scripts/utils/ApiClient.js
+++ web/react_chat/assets/scripts/utils/ApiClient.js
@@ -1,30 +1,44 @@
-import socketClient from 'socket.io-client';
+import SockJS from 'sockjs-client';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from 'app/actions/conversation';
 
-let socket;
+var sock;
 
 export function sendMessage(conversationId, message) {
-  socket.emit('SEND_MESSAGE', conversationId, message);
+  //sock.emit('SEND_MESSAGE', conversationId, message);
+  sock.send('SEND_MESSAGE');
 }
 
 export function requestMessages(conversationId, callback) {
-  socket.emit('REQUEST_MESSAGES', conversationId, callback);
+  //sock.emit('REQUEST_MESSAGES', conversationId, callback);
 }
 
-export function bindActions(socketUrl, id, dispatch) {
-  socket = socketClient(socketUrl);
-  const actions = bindActionCreators(actionCreators, dispatch);
-  socket.on('connect', function() {
-    console.log('connected', arguments);
-    socket.emit('register', id);
-  });
-  socket.on('disconnect', function() {
-    console.log('disconnected', arguments);
-  });
-  socket.on('message', function(payload) {
-    console.log('got', arguments);
-    const { conversation_id: conversationId, message } = payload;
-    actions.receiveMessage({ conversationId, message });
-  });
+export function bindActions(url, id, dispatch) {
+  sock = new SockJS(url);
+  sock.onopen = function() {
+    console.log('open');
+    sock.send({q:'w',e:'r',t:'y'});
+  };
+  sock.onmessage = function(event) {
+    console.log('message', event.data);
+    console.log(event);
+  };
+  sock.onclose = function() {
+    console.log('close');
+  };
+  //sock.close();
+  //socket = socketClient(socketUrl);
+  //const actions = bindActionCreators(actionCreators, dispatch);
+  //socket.on('connect', function() {
+  //  console.log('connected', arguments);
+  //  socket.emit('register', id);
+  //});
+  //socket.on('disconnect', function() {
+  //  console.log('disconnected', arguments);
+  //});
+  //socket.on('message', function(payload) {
+  //  console.log('got', arguments);
+  //  const { conversation_id: conversationId, message } = payload;
+  //  actions.receiveMessage({ conversationId, message });
+  //});
 }
