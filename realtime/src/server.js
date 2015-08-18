313cd43036b52810d69e253b242caed52c7c5878
Slava Shklyaev <shk.slava@gmail.com>
2015-08-18 18:52:20 +0300

    realtime: lint

--- realtime/src/server.js
+++ realtime/src/server.js
@@ -6,62 +6,59 @@ const DEBUG = process.env.ENVIRONMENT === 'development';
 const userSparks = {};
 
 function showSpark(spark) {
-  return (spark.user_id || '?') + '@' + spark.id.slice(0, 4);
+  return (spark.userId || '?') + '@' + spark.id.slice(0, 4);
 }
 
-function showUserSparks(userSparks) {
-  return Object.keys(userSparks).reduce((arr, user_id) => {
-    return arr.concat(userSparks[user_id].map(showSpark));
+function showUserSparks(sparks) {
+  return Object.keys(sparks).reduce((arr, userId) => {
+    return arr.concat(userSparks[userId].map(showSpark));
   }, []).join(', ');
 }
 
 function handleMessage(spark, data) {
   if (DEBUG) console.log(showSpark(spark), data);
   if (data.type === 'REGISTER') {
-    const { user_id } = data.payload;
-    spark.user_id = user_id;
-    if (userSparks[user_id]) {
-      userSparks[user_id].push(spark);
-    }
-    else {
-      userSparks[user_id] = [spark];
+    const { user_id: userId } = data.payload;
+    spark.userId = userId;
+    if (userSparks[userId]) {
+      userSparks[userId].push(spark);
+    } else {
+      userSparks[userId] = [spark];
     }
     if (DEBUG) console.log('sparks:', showUserSparks(userSparks));
   }
 }
 
 function handleDisconnection(spark) {
-  const { user_id } = spark;
-  if (!userSparks[user_id]) {
+  const { userId } = spark;
+  if (!userSparks[userId]) {
     return;
   }
   if (DEBUG) console.log(showSpark(spark), 'disconnected');
-  const remainingSparks = userSparks[user_id].filter(x => x !== spark);
+  const remainingSparks = userSparks[userId].filter(x => x !== spark);
   if (remainingSparks.length === 0) {
-    delete userSparks[user_id];
-  }
-  else {
-    userSparks[user_id] = remainingSparks;
+    delete userSparks[userId];
+  } else {
+    userSparks[userId] = remainingSparks;
   }
   if (DEBUG) console.log('sparks:', showUserSparks(userSparks));
 }
 
-function handleNotification({ args: [user_ids, payload] }) {
-  user_ids = user_ids.map(String);
-  user_ids.forEach(user_id => {
-    if (userSparks[user_id]) {
-      userSparks[user_id].forEach(spark => spark.write(payload));
+function handleNotification({ args: [userIds, payload] }) {
+  userIds.forEach(userId => {
+    if (userSparks[userId]) {
+      userSparks[userId].forEach(spark => spark.write(payload));
     }
   });
-  if (DEBUG) console.log('notified', user_ids, payload);
+  if (DEBUG) console.log('notified', userIds, payload);
 }
 
-function run() {
+export function run() {
   const primus = Primus.createServer({
     port: process.env.PORT || 3000,
     transformer: 'sockjs',
   });
-  primus.on('connection', function(spark) {
+  primus.on('connection', spark => {
     spark.on('data', data => handleMessage(spark, data));
     spark.on('end', () => handleDisconnection(spark));
   });
@@ -71,9 +68,11 @@ function run() {
     login: process.env.QUEUE_USER,
     password: process.env.QUEUE_PASSWORD,
   });
-  queueClient.on('ready', function() {
-    const queueOptions = { durable: true, autoDelete: false };
-    queueClient.queue('notifications', queueOptions, function(queue) {
+  queueClient.on('ready', () => {
+    queueClient.queue('notifications', {
+      durable: true,
+      autoDelete: false,
+    }, queue => {
       queue.bind('#');
       queue.subscribe(handleNotification);
     });
--- web/react_chat/tasks.py
+++ web/react_chat/tasks.py
@@ -2,4 +2,5 @@ from celery import shared_task
 
 @shared_task(ignore_result=True, queue='notifications', serializer='json')
 def notify_users(user_ids, data):
+    # TODO: send user_ids as strings
     raise NotImplementedError
