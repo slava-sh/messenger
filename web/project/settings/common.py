2630739d2ec65e9ba035540634efafcfba78c831
Slava Shklyaev <shk.slava@gmail.com>
2015-08-24 18:39:55 +0300

    web: enable Django sites framework

--- web/project/settings/common.py
+++ web/project/settings/common.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
     'storages',
     'registration',
