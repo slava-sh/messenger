772517026a31265048d7898748b5dff0c44c12fd
Slava Shklyaev <shk.slava@gmail.com>
2015-08-27 08:55:27 +0300

    web: change gravatar size to 80x80

--- web/api/utils.py
+++ web/api/utils.py
@@ -2,6 +2,6 @@ import hashlib
 
 GRAVATAR_URL_TEMPLATE = 'https://www.gravatar.com/avatar/{hash}?s={size}'
 
-def gravatar_url(email, size=40):
+def gravatar_url(email, size=80):
     hash = hashlib.md5(email.lower().encode()).hexdigest()
     return GRAVATAR_URL_TEMPLATE.format(hash=hash, size=size)
