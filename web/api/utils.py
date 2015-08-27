c0e090b7c7bc2fe200e93a538b77555b51a96e4d
Slava Shklyaev <shk.slava@gmail.com>
2015-08-27 20:23:19 +0300

    web: tweak gravatars

--- web/api/utils.py
+++ web/api/utils.py
@@ -1,6 +1,7 @@
 import hashlib
 
-GRAVATAR_URL_TEMPLATE = 'https://www.gravatar.com/avatar/{hash}?s={size}'
+GRAVATAR_URL_TEMPLATE = ('https://secure.gravatar.com/avatar/{hash}'
+                         '?s={size}&d=wavatar')
 
 def gravatar_url(email, size=80):
     hash = hashlib.md5(email.lower().encode()).hexdigest()
