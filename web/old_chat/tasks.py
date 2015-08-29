37bb9674f63149ceb94632722cb67700ef9fdb7c
Slava Shklyaev <shk.slava@gmail.com>
2015-08-29 04:26:20 +0300

    worker: create first conversation and add a message

--- web/old_chat/tasks.py
+++ web/old_chat/tasks.py
@@ -4,4 +4,9 @@ from .models import Conversation, Message
 
 @shared_task(ignore_result=True)
 def create_first_conversation(user_pk):
-    conversation = Conversation.objects.create(name='hey') #, members=[1, user_pk])
+    message = Message.objects.get(pk=1)
+    conversation = Conversation.objects.create(name=message.author.username)
+    conversation.members.add(user_pk, message.author)
+    message.pk = None
+    message.conversation = conversation
+    message.save()
