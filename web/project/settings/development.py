d8500aec1f39ff0370936bc0a86f6d386e56a9fb
Slava Shklyaev <shk.slava@gmail.com>
2015-09-02 23:44:19 +0300

    web: serve static files with whitenoise

--- web/project/settings/development.py
+++ web/project/settings/development.py
@@ -4,5 +4,7 @@ DEBUG = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-STATIC_ROOT = '/tmp/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
