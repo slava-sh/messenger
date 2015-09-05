078b932a938e6e1bc5cb3d517d1ca9aceece09ce
Slava Shklyaev <shk.slava@gmail.com>
2015-09-05 18:34:47 +0300

    chat -> messenger

--- web/project/celery.py
+++ web/project/celery.py
@@ -5,6 +5,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 from django.conf import settings
 from celery import Celery
 
-app = Celery('chat')
+app = Celery()
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
