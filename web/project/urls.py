f4a76ea3f660452747ace61c1a8ed5a6e71b8a8a
Slava Shklyaev <shk.slava@gmail.com>
2015-09-05 16:14:35 +0300

    web: delete old_chat views

--- web/project/urls.py
+++ web/project/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
-    url(r'^old/', include('messaging.urls', namespace='messaging')),
     url(r'^api/', include('api.urls', namespace='api')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
