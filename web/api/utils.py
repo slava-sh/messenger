5e3fd3edcca308b0d35991e5ce4789e73d291b12
Slava Shklyaev <shk.slava@gmail.com>
2015-10-30 19:47:22 +0200

    Add avatars

--- web/api/utils.py
+++ web/api/utils.py
@@ -4,5 +4,11 @@ GRAVATAR_URL_TEMPLATE = ('https://secure.gravatar.com/avatar/{hash}'
                          '?s={size}&d=wavatar')
 
 def gravatar_url(email, size=80):
+    # These are from https://blog.placeit.net/free-avatar-pack/
+    if email == 'dana@localhost':
+        return '/035f.jpg'
+    if email == 'mark@localhost':
+        return '/081m.jpg'
+
     hash = hashlib.md5(email.lower().encode()).hexdigest()
     return GRAVATAR_URL_TEMPLATE.format(hash=hash, size=size)
